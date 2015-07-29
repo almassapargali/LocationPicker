d67494ed92cca0aa4878b5115dac537ba3218802
Almas Sapargali <almassapargali@gmail.com>
2015-07-29 21:50:56 +0600

    store user selected location too

--- LocationPicker/LocationPicker/Location.swift
+++ LocationPicker/LocationPicker/Location.swift
@@ -14,14 +14,19 @@ import AddressBookUI
 // class because protocol
 public class Location: NSObject {
 	public let name: String?
+	
+	// difference from placemark location is that if location was reverse geocoded,
+	// then location point to user selected location
+	public let location: CLLocation
 	public let placemark: CLPlacemark
 	
 	var address: String {
 		return ABCreateStringWithAddressDictionary(placemark.addressDictionary, true)
 	}
 	
-	init(name: String?, placemark: CLPlacemark) {
+	init(name: String?, location: CLLocation? = nil, placemark: CLPlacemark) {
 		self.name = name
+		self.location = location ?? placemark.location
 		self.placemark = placemark
 	}
 }
@@ -30,7 +35,7 @@ import MapKit
 
 extension Location: MKAnnotation {
     @objc public var coordinate: CLLocationCoordinate2D {
-		return placemark.location.coordinate
+		return location.coordinate
 	}
 	
     public var title: String {
