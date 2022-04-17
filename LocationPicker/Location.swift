759fb9062afe80f13eae33a2d5ad91ae3b2dead4
Izumi Higa <higalex@gmail.com>
2022-04-17 08:50:39 -0400

    Fix deprecated warnings

--- LocationPicker/Location.swift
+++ LocationPicker/Location.swift
@@ -10,6 +10,7 @@ import Foundation
 
 import CoreLocation
 import AddressBookUI
+import Contacts
 
 // class because protocol
 public class Location: NSObject {
@@ -20,19 +21,16 @@ public class Location: NSObject {
 	public let location: CLLocation
 	public let placemark: CLPlacemark
 	
-	public var address: String {
-		// try to build full address first
-		if let addressDic = placemark.addressDictionary {
-			if let lines = addressDic["FormattedAddressLines"] as? [String] {
-				return lines.joined(separator: ", ")
-			} else {
-				// fallback
-				return ABCreateStringWithAddressDictionary(addressDic, true)
-			}
-		} else {
-			return "\(coordinate.latitude), \(coordinate.longitude)"
-		}
-	}
+    public var address: String {
+        // try to build full address first
+        if let addressDic = placemark.addressDictionary, let lines = addressDic["FormattedAddressLines"] as? [String] {
+            return lines.joined(separator: ", ")
+        }
+        if #available(iOS 11.0, *), let postalAddress = placemark.postalAddress {
+            return CNPostalAddressFormatter().string(from: postalAddress)
+        }
+        return "\(coordinate.latitude), \(coordinate.longitude)"
+    }
 	
 	public init(name: String?, location: CLLocation? = nil, placemark: CLPlacemark) {
 		self.name = name
